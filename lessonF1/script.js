7185d27081e02578c7386408789f22fa34d31629
victoriowayne <95044163+victoriowayne@users.noreply.github.com>
2021-11-25 19:44:36 +0300

    Add files via upload

---
+++ lessonF1/script.js
@@ -0,0 +1,17 @@
+VANTA.BIRDS({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x5e4472
+  })
+
+
+// Подключить все три скрипта в html 
+// <script src="three.min.js"></script>
+// <script src="vanta.birds.min.js"></script>
+// <script src="script.js"></script>
